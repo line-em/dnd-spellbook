20e987245c0d4d65b4d64d99a6add6005f372cbb
Line-em <alineemily@hotmail.com>
2022-11-21 16:41:06 +0000

    Add Home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,31 @@
+import Heading from "../styled-components/Heading";
+import StyledButton from "../styled-components/StyledButton";
+import { LogoImage250 as LogoImage } from "../styled-components/LogoImage";
+import styled from "styled-components";
+
+const FlexColumnWrapper = styled.section`
+	display: flex;
+	flex-direction: column;
+	gap: var(--sm);
+`;
+
+const CenterFlexColumn = styled(FlexColumnWrapper)`
+	place-items: center;
+	text-align: center;
+`;
+function Home() {
+	return (
+		<CenterFlexColumn>
+			<LogoImage />
+			<Heading type="1">DnD Spellbook</Heading>
+			<Heading type="3">How do you want to inspect this tome?</Heading>
+			<p>
+				You can filter through classes and schools, or use the advanced feature to type or
+				check mana costs and duration.
+			</p>
+			<StyledButton func={(e) => console.log("oi")}>Begin</StyledButton>
+		</CenterFlexColumn>
+	);
+}
+
+export default Home;
