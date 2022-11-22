7d1b3f2a0b94183c6c157c4b635d146ed7c16c39
Line-em <alineemily@hotmail.com>
2022-11-22 11:16:54 +0000

    add schools

--- src/assets/schools/schoolsData.js
+++ src/assets/schools/schoolsData.js
@@ -1,18 +1,34 @@
 export default [
 	{
 		class: "Abjuration",
-		symbol: "./src/assets/schools/Abjuration.png"
+		symbol: "./src/assets/schools/Abjuration2.png"
 	},
 	{
 		class: "Conjuration",
 		symbol: "./src/assets/schools/Conjuration.png"
 	},
 	{
-		class: "Abjuration",
-		symbol: "./src/assets/schools/Abjuration.png"
+		class: "Divination",
+		symbol: "./src/assets/schools/Divination.png"
 	},
 	{
-		class: "Abjuration",
-		symbol: "./src/assets/schools/Abjuration.png"
+		class: "Enchantment",
+		symbol: "./src/assets/schools/Enchantment.png"
+	},
+	{
+		class: "Evocation",
+		symbol: "./src/assets/schools/Evocation.png"
+	},
+	{
+		class: "Illusion",
+		symbol: "./src/assets/schools/Illusion.png"
+	},
+	{
+		class: "Necromancy",
+		symbol: "./src/assets/schools/Necromancy.png"
+	},
+	{
+		class: "Transmutation",
+		symbol: "./src/assets/schools/Transmutation.png"
 	}
 ];
--- src/pages/Filters.jsx
+++ src/pages/Filters.jsx
@@ -3,11 +3,13 @@ import { WhiteSection } from "../styled-components/FlexStyles";
 import classesData from "../assets/classes/classesData.js";
 import schoolsData from "../assets/schools/schoolsData";
 import styled from "styled-components";
+import { StyledLinkButton } from "../styled-components/StyledButton";
 
 // Filter grid - 2 columns
 
 const ClassBubble = styled.button`
 	border-radius: 50%;
+	aspect-ratio: 1 / 1;
 	width: 50%;
 	background-color: var(--transparent-black);
 	border: 2px var(--dark-gray) solid;
@@ -24,12 +26,20 @@ const ClassBubble = styled.button`
 	}
 `;
 
+const SchoolBubble = styled(ClassBubble)`
+	& img {
+		width: 95%;
+		height: auto;
+	}
+`;
+
 const StyledFilter = ({ children }) => <ClassBubble>{children}</ClassBubble>;
+const StyledFilterSchool = ({ children }) => <SchoolBubble>{children}</SchoolBubble>;
 
 const FilterGrid = styled.section`
 	display: grid;
 	gap: var(--xs);
-	grid-template-columns: ${({ column }) => `repeat(${column}, minmax(100px, 150px))`};
+	grid-template-columns: ${({ column }) => `repeat(${column}, minmax(100px, 120px))`};
 `;
 
 const Filters = (props) => {
@@ -38,16 +48,16 @@ const Filters = (props) => {
 			<StyledFilter>
 				<img src={clss.symbol} alt={clss.class} key={index} />
 			</StyledFilter>
-			<Heading type="6">{clss.class}</Heading>
+			<p style={{ fontSize: "14px" }}>{clss.class}</p>
 		</div>
 	));
 
 	const schoolMap = schoolsData.map((clss, index) => (
 		<div style={{ maxWidth: "150px" }}>
-			<StyledFilter>
+			<StyledFilterSchool>
 				<img src={clss.symbol} alt={clss.class} key={index} />
-			</StyledFilter>
-			<Heading type="6">{clss.class}</Heading>
+			</StyledFilterSchool>
+			<p style={{ fontSize: "14px" }}>{clss.class}</p>
 		</div>
 	));
 
@@ -55,16 +65,18 @@ const Filters = (props) => {
 		<WhiteSection style={{ width: "100%" }}>
 			<Heading type="2">The Spellbook</Heading>
 			<p>Please select the filters you'd like to apply, and click on Search.</p>
-			<section style={{ display: "grid", gridTemplateColumns: "repeat(4, auto)" }}>
+			<WhiteSection style={{ display: "grid", gridTemplateColumns: "repeat(4, auto)" }}>
 				<div>
 					<Heading type="4">Classes</Heading>
 					<FilterGrid column="4">{classMap}</FilterGrid>
 				</div>
+				-
 				<div>
 					<Heading type="4">Schools</Heading>
-					<FilterGrid column="6">{schoolMap}</FilterGrid>
+					<FilterGrid column="4">{schoolMap}</FilterGrid>
 				</div>
-			</section>
+			</WhiteSection>
+			<StyledLinkButton>Search</StyledLinkButton>
 		</WhiteSection>
 	);
 };
