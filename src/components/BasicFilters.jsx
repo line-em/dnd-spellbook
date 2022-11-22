5b42caf9e175ba80947731622ed663561502d04d
Line-em <alineemily@hotmail.com>
2022-11-22 16:01:23 +0000

    Visuals done, ready to make query for API

--- src/components/BasicFilters.jsx
+++ src/components/BasicFilters.jsx
@@ -1,32 +1,23 @@
 import classesData from "../assets/classes/classesData.js";
 import schoolsData from "../assets/schools/schoolsData";
-import Heading from "../styled-components/Heading";
-import { StyledButton } from "../styled-components/StyledButton";
-import { StyledFilter } from "../styled-components/ClassBubble";
-import { FilterGrid, WhiteSection } from "../styled-components/FlexStyles";
+import { WhiteSection } from "../styled-components/FlexStyles";
+import { useState } from "react";
+import BasicFiltersLogic from "./BasicFiltersLogic.jsx";
 
 const BasicFilters = () => {
-	const mapFilters = (filterArray) =>
-		filterArray.map((filter, index) => (
-			<div>
-				{/* selected */}
-				<StyledFilter isImg={!filter.icon}>
-					{filter.icon ? (
-						<img src={filter.icon} alt={filter.class} key={index} />
-					) : (
-						<p>{filter.class}</p>
-					)}
-				</StyledFilter>
-				{filter.icon ? <p>{filter.class}</p> : ""}
-			</div>
-		));
+	const [isSelected, setIsSelected] = useState(false);
+
+	const handleSelected = (selection) => console.log((prev) => [...prev, selection]);
+
 	return (
 		<WhiteSection>
-			<Heading type="4">Classes</Heading>
-			<FilterGrid>{mapFilters(classesData)}</FilterGrid>
+			<BasicFiltersLogic title="Classes" filterArray={classesData} initialState={true} />
 			<hr />
-			<Heading type="4">Schools of Magic</Heading>
-			<FilterGrid>{mapFilters(schoolsData)}</FilterGrid>
+			<BasicFiltersLogic
+				title="Schools of Magic"
+				filterArray={schoolsData}
+				initialState={false}
+			/>
 		</WhiteSection>
 	);
 };
