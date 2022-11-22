57c68362b656bc8750add81a0accc87a16a7f8dd
Line-em <alineemily@hotmail.com>
2022-11-22 14:08:42 +0000

    separation of Filter page and Basic Filters Component

--- src/pages/Filters.jsx
+++ src/pages/Filters.jsx
@@ -1,51 +1,14 @@
 import Heading from "../styled-components/Heading";
-import {
-	FilterGrid,
-	FlexRowWrapper,
-	SpellbookPage,
-	WhiteSection
-} from "../styled-components/FlexStyles";
-import classesData from "../assets/classes/classesData.js";
-import schoolsData from "../assets/schools/schoolsData";
-import { StyledButton, StyledLinkButton } from "../styled-components/StyledButton";
-import { StyledFilter } from "../styled-components/ClassBubble";
-import styled from "styled-components";
+import { FlexRowWrapper, SpellbookPage } from "../styled-components/FlexStyles";
+import { StyledLinkButton } from "../styled-components/StyledButton";
+import BasicFilters from "../components/BasicFilters";
 
 const Filters = (props) => {
-	const mapFilters = (filterArray) =>
-		filterArray.map((filter, index) => (
-			<div>
-				{/* selected */}
-				<StyledFilter isImg={filterArray === schoolsData}>
-					{filterArray === schoolsData ? (
-						<p>{filter.class}</p>
-					) : (
-						<img src={filter.symbol} alt={filter.class} key={index} />
-					)}
-				</StyledFilter>
-				{filterArray === schoolsData ? "" : <p>{filter.class}</p>}
-			</div>
-		));
-
 	return (
 		<SpellbookPage>
 			<Heading type="2">The Spellbook</Heading>
 			<p>Select the filters you'd like to apply, and click on Search.</p>
-			<WhiteSection>
-				<Heading type="4">Classes</Heading>
-				<FilterGrid column="4">
-					{mapFilters(classesData)}
-					<a>All Classes</a>
-				</FilterGrid>
-
-				<hr />
-
-				<Heading type="4">Schools of Magic</Heading>
-				<FilterGrid column="4">
-					{mapFilters(schoolsData)}
-					<StyledButton>All Schools</StyledButton>
-				</FilterGrid>
-			</WhiteSection>
+			<BasicFilters />
 			<FlexRowWrapper>
 				<StyledLinkButton path="/">Home</StyledLinkButton>
 				<StyledLinkButton>Search</StyledLinkButton>
