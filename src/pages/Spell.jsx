2d25674d9b143b1a3240795fba7701c6e3cd5316
Line-em <alineemily@hotmail.com>
2022-12-28 21:06:28 +0000

    still lots to refactor. starting to customize the spell page

--- src/pages/Spell.jsx
+++ src/pages/Spell.jsx
@@ -1,48 +1,57 @@
 // const found = array1.find((element) => element > 10);
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { useContext, useEffect, useState } from "react";
 import { capitalize, sanitizeClasses } from "../utils/utils";
 import { WhiteSectionBackdropLeft } from "../styled-components/SearchUtils";
 import { WhiteSection } from "../styled-components/FlexStyles";
+import Heading from "../styled-components/Heading";
+import RowButtons from "../components/RowButtons";
 
 const Spell = () => {
 	const { slug } = useParams();
-	let currentSpell;
-	if (localStorage.getItem("api")) {
-		const localSpells = JSON.parse(localStorage.getItem("api"));
-		currentSpell = localSpells.find((spell) => spell.slug === slug);
-		console.log(currentSpell);
-	} else {
-		console.log("need new api call directly to spells");
-		// Only for avoiding problems in case the person skipped the Filters page.
-		useEffect(() => {
-			const fetchUniqueSpell = async () => {
+	const { navigate } = useNavigate();
+	const [spell, setSpell] = useState([]);
+	const { name, desc, school, dnd_class } = spell;
+
+	useEffect(() => {
+		if (localStorage.getItem("api")) {
+			// FIXME:not tested
+			const localSpells = JSON.parse(localStorage.getItem("api"));
+			let spellObj = localSpells.find((spell) => spell.slug === slug);
+			return setSpell(spellObj);
+		} else {
+			const getSpell = async () => {
 				try {
 					const url = `${import.meta.env.VITE_URL}?slug=${slug}`;
 					const response = await axios.get(url);
 					const results = await response.data.results;
 					results[0].dnd_class = sanitizeClasses(results[0].dnd_class);
-					return results;
+					return setSpell(results[0]);
 				} catch (error) {
 					console.log(error);
 				}
 			};
-			fetchUniqueSpell();
-		}, []);
-	}
+			getSpell();
+		}
+	}, []);
 
 	return (
 		<>
 			<WhiteSection maxWidth="90ch">
-				{/* <Heading type="1">{currentSpell.name}</Heading>
+				<Heading type="1">{name}</Heading>
+				<WhiteSection>{dnd_class.map((classes) => classes)}</WhiteSection>
 				<WhiteSection>
-					<p>{currentSpell.desc}</p>
-				</WhiteSection> */}
+					<p>{desc}</p>
+					<p>{school}</p>
+				</WhiteSection>
+				<WhiteSection>
+					<RowButtons buttonText={"Return"} handleFunction={() => navigate("/results")} />
+				</WhiteSection>
 			</WhiteSection>
-			<WhiteSectionBackdropLeft
-			// school={capitalize(currentSpell.school)}
-			></WhiteSectionBackdropLeft>
+			{school && (
+				<WhiteSectionBackdropLeft school={capitalize(school)}></WhiteSectionBackdropLeft>
+			)}
 		</>
 	);
 };
