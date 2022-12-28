e1dcc778af7ac363ae5705971128d98d725b1b99
Line-em <alineemily@hotmail.com>
2022-12-28 16:57:23 +0000

    localStorage hook added, now to fix the Spells page

--- src/context/ApiContext.jsx
+++ src/context/ApiContext.jsx
@@ -1,40 +1,37 @@
 import { createContext, useEffect, useState } from "react";
 import { sanitizeClasses } from "../utils/utils";
 import axios from "axios";
+import useLocalStorage from "../hooks/useLocalStorage";
 
 export const ApiContext = createContext();
 
 export const ApiContextProvider = ({ children }) => {
-	const [apiData, setApiData] = useState([]);
+	const [apiData, setApiData] = useLocalStorage("api", "testing");
+
+	const fetchSpells = async (url) => {
+		const data = [];
+		try {
+			do {
+				const response = await axios.get(url);
+				const res = await response.data;
+				res.results.forEach(
+					(result) => (result.dnd_class = sanitizeClasses(result.dnd_class))
+				);
+				data.push(...res.results);
+				url = res.next;
+			} while (url);
+			return setApiData(data);
+		} catch (err) {
+			console.log(err);
+		}
+	};
 
 	useEffect(() => {
-		if (JSON.parse(localStorage.getItem("api")).length > 0) {
-			setApiData(JSON.parse(localStorage.getItem("api")));
-		} else {
-			console.log("FETCHING THE API!");
-			setApiData(fetchAllPages("https://api.open5e.com/spells/"));
-			localStorage.setItem(
-				"api",
-				JSON.stringify(fetchAllPages("https://api.open5e.com/spells/"))
-			);
+		if (!localStorage.getItem("api")) {
+			console.log("fetching the api");
+			return fetchSpells("https://api.open5e.com/spells/");
 		}
 	}, []);
 
 	return <ApiContext.Provider value={apiData}>{children}</ApiContext.Provider>;
 };
-
-async function fetchAllPages(url) {
-	const data = [];
-	try {
-		do {
-			const response = await axios.get(url);
-			const res = await response.data;
-			res.results.forEach((result) => (result.dnd_class = sanitizeClasses(result.dnd_class)));
-			data.push(...res.results);
-			url = res.next;
-		} while (url);
-		return data;
-	} catch (err) {
-		console.log(err);
-	}
-}
