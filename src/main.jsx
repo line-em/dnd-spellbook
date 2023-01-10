f4422e8ad5b7507792a128a60ba25835282911b4
Line-em <alineemily@hotmail.com>
2023-01-10 20:19:03 +0000

    created globalStyles to use only styled components

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import App from "./App";
-import { GlobalStyle } from "./GlobalStyles";
+import { GlobalStyle } from "./globalStyles";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<HashRouter>
