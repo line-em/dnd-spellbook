b45caa01f94bc2c38d5e90464de670641b62fbc5
Line-em <alineemily@hotmail.com>
2023-04-16 16:58:40 +0100

    add images

---
+++ dist/assets/schools/schoolsData.js
@@ -0,0 +1,45 @@
+export default [
+	{
+		class: "Abjuration",
+		query: "Abjuration",
+		symbol: "./assets/schools/Abjuration2.png"
+	},
+	{
+		class: "Conjuration",
+		query: "Conjuration",
+		symbol: "./assets/schools/Conjuration.png"
+	},
+	{
+		class: "Divination",
+		query: "Divination",
+		symbol: "./assets/schools/Divination.png"
+	},
+	{
+		class: "Enchantment",
+		query: "Enchantment",
+		symbol: "./assets/schools/Enchantment.png"
+	},
+	{
+		class: "Evocation",
+		query: "Evocation",
+		symbol: "./assets/schools/Evocation.png"
+	},
+	{
+		class: "Illusion",
+		query: "Illusion",
+		symbol: "./assets/schools/Illusion.png"
+	},
+	{
+		class: "Necromancy",
+		query: "Necromancy",
+		symbol: "./assets/schools/Necromancy.png"
+	},
+	{
+		class: "Transmutation",
+		query: "Transmutation",
+		symbol: "./assets/schools/Transmutation.png"
+	},
+	{
+		class: "🧼 Clear Filters"
+	}
+];
